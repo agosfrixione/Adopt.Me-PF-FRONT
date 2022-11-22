e8b05a0befb6349680d4cb7e962ecbdf1120956d
agosfrixione <agosfrixione@gmail.com>
2022-11-22 16:45:02 -0300

    actualizacion de import de elemnto AdoptCat y AdoptDog

--- front/src/Container/App.js
+++ front/src/Container/App.js
@@ -2,10 +2,11 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingPage from '../Componentes/LandingPage/LandingPage';
 import HomePage from '../Componentes/HomePage/HomePage';
-import AdoptCat from '../Componentes/AdoptarGato/AdoptarGato';
-import AdoptDog from '../Componentes/AdoptarPerro/AdoptarPerro';
+import AdoptCat from '../Componentes/AdoptarMascota/HomeGatos';
+import AdoptDog from '../Componentes/AdoptarMascota/HomePerros';
 import InfoProceso from '../Componentes/DonarMascota/InfoProceso';
 import FormRegistro from '../Componentes/FormRegistro/FormRegistro';
+import DetalleAdopcion from '../Componentes/AdoptarMascota/DetalleAdopcion'
 import Confirmacion from '../Componentes/DonarMascota/Confirmacion';
 import AnimalAbuse from '../Componentes/DenunciarMaltrato/DenunciarMaltrato';
 import OtherThings from '../Componentes/TePuedeInteresar/TePuedeInteresar';
@@ -21,6 +22,7 @@ function App() {
     <Route exact path = '/adoptdog' element = {<AdoptDog/>} />
     <Route exact path = '/givepet' element = {<InfoProceso/>} />
     <Route exact path = '/sigup' element = {<FormRegistro/>} />
+    <Route exact path = '/contacto' element = {<DetalleAdopcion/>} />
     <Route exact path = '/confirmation' element = {<Confirmacion/>} />
     <Route exact path = '/animalabuse' element = {<AnimalAbuse/>} />
     <Route exact path = '/otherthings' element = {<OtherThings/>} />
