53249d17a3975e0d2bd2c0222c73c0321df02bb1
agosfrixione <agosfrixione@gmail.com>
2022-11-22 16:10:52 -0300

    reorganizacion de componente AdoptarMascota y del formulario de registro

--- front/src/Componentes/AdoptarMascota/DetalleAdopcion.js
+++ front/src/Componentes/AdoptarMascota/DetalleAdopcion.js
@@ -26,4 +26,4 @@ export default function InfoProceso () {
             </Link>
         </div>
     );
-};
+}; 
---
+++ front/src/Componentes/AdoptarMascota/DetallePerro.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function DetallePerro () {
+    return (
+
+        <div>Detalle de perro</div>
+    )
+}
