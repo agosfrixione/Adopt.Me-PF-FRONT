44946ba9cf01f1b753d904783528358a7cfb37d0
Andres <andrescantlon@gmail.com>
2022-11-22 21:00:15 -0300

    action y cors

--- BACK/index.js
+++ BACK/index.js
@@ -2,13 +2,13 @@ const express = require ('express');
 require('../BACK/src/db');
 const app = express ();
 const animalRutas = require('../BACK/src/rutas/animales');
-
+const cors = require('cors')
 const usuarioRutas = require('../BACK/src/rutas/usuarios');
 
 const morgan = require('morgan')
 
 
-
+app.use(cors());
 app.set ('port', process.env.PORT || 3001);
 app.use(express.json());
 app.use(morgan('dev'));
--- front/src/Reducer/index.js
+++ front/src/Reducer/index.js
@@ -1,10 +1,20 @@
+import { GET_MASCOTAS } from "../Actions";
+
 const initialState = {
-   
+   animales: []
 }
 
 export default function rootReducer(state = initialState, action){
     switch(action.type){
 
+        case GET_MASCOTAS:     
+        if(action.payload) { 
+            return {       
+                ...state,   
+                animales: action.payload
+            }              
+        } else { 
+            return { ...state, animales:[] } }
 
         default:
             return state;
