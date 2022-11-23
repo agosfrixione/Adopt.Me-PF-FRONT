292f1f7793de7ddd1595606ad0fd8932c51ef879
norato777 <norato777@gmail.com>
2022-11-23 11:12:43 -0600

    updating

---
+++ front/src/Componentes/BuscarMascotaPerdida/BuscarMascota.js
@@ -0,0 +1,6 @@
+import React from "react";
+import UploadImages from "../UploadImages/UploadImages";
+
+export default function BuscarMascota() {
+  return <UploadImages />;
+}
---
+++ front/src/Componentes/UploadImages/UploadImages.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const UploadImages = (props) => {
+  return <div>prueba imagenes</div>;
+};
