493e8a88c6ca23082ad664a6b5085db03e7fafc4
NachoAbait <102827148+NachoAbait@users.noreply.github.com>
2022-11-23 17:42:32 -0300

    Creacion de metodos para hashear la contraseña y compararlas

--- BACK/src/modelos/usuarios.js
+++ BACK/src/modelos/usuarios.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const bcrypt = require("bcryptjs");
 
 const UsuarioSchema = new Schema({
   nombre: {
@@ -15,18 +16,31 @@ const UsuarioSchema = new Schema({
     required: true,
   },
   localidad: {
-    type: String,
-    required: true,
+    type: String,/*
+    required: true,*/
   },
   nacimiento: {
-    type: Date,
-    required: true,
+    type: Date,/*
+    required: true,*/
   },
   publicaciones: {
     type: String,
   },
 });
 
-const UsuarioModel = mongoose.model('usuarios', UsuarioSchema)
+// METODO PARA HASHEAR CONTRASEÑA
+UserSchema.methods.encryptPassword = async (contraseña) => {
+  const salt = await bcrypt.genSalt(10); // veces q aplica el algoritmo para obtener el hash
+  const hash = bcrypt.hash(contraseña, salt); // combinamos hash y contraseña para encriptarla
+  return hash
+}
 
+// METODO PARA TOMAR LA CONTRASEÑA Y HASHEARLA PARA COMPARARLA CON LA HASHEADA EN LA DB
+UserSchema.methods.compararContraseña = async function(contraseña){
+  return await bcrypt.compare(contraseña, this.contraseña)
+}
+
+
+
+const UsuarioModel = mongoose.model('usuarios', UsuarioSchema)
 module.exports = UsuarioModel;
