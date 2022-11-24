0c06716dba2d12ca2ac22be45d38fac7eff4cdeb
norato777 <norato777@gmail.com>
2022-11-24 11:30:16 -0600

    upload actualizado

--- front/src/Componentes/UploadImages/UploadImages.js
+++ front/src/Componentes/UploadImages/UploadImages.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
-import axios from "axios";
 
 export default function UploadImages(props) {
   const { image, setImage } = useState("");
@@ -8,20 +7,24 @@ export default function UploadImages(props) {
 
   const uploadImage = async (e) => {
     const files = e.target.files;
-    const data = new Form();
+    const data = new FormData();
     data.append("file", files[0]);
     data.append("upload_preset", "mascotas");
     setLoading(true);
-    const res = await axios(
-      "https://api.cloudinary.com/v1_1/dvw0vrnxp/mascotas/upload",
+    const res = await fetch(
+      "https://api.cloudinary.com/v1_1/dvw0vrnxp/image/upload",
       {
         method: "POST",
         body: data,
       }
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((err) => {
+          console.log(err);
+        })
     );
-    const file = await res.json();
-    console.log(res);
-    setImage(file.secure_url);
     setLoading(false);
   };
 
@@ -30,12 +33,13 @@ export default function UploadImages(props) {
       <div style={{ textAlign: "center" }}>
         <h1>Subiendo Imagenes</h1>
         <Form>
-          <input
-            type="file"
-            name="file"
-            placeholder="Agrega Imagenes"
-            onChange={uploadImage}
-          />
+          <input type="file" name="file" placeholder="Agrega Imagenes" />
+          <button onClick={uploadImage}>UPLOAD</button>
+          {loading ? (
+            <h3>Cargando imagenes...</h3>
+          ) : (
+            <img src={image} style={{ width: "300px" }} />
+          )}
         </Form>
       </div>
     </div>
