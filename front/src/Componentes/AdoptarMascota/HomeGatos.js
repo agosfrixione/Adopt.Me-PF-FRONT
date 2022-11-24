b688167d16936eaab7567e27dfcff5cb904a3123
@Noesneda <noe_gal_08u@hotmail.com>
2022-11-24 19:45:39 -0300

    Renderizado de Home Gatos

--- front/src/Componentes/AdoptarMascota/HomeGatos.js
+++ front/src/Componentes/AdoptarMascota/HomeGatos.js
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { useEffect } from "react";
+import {useDispatch, useSelector} from 'react-redux';
+import { Link } from "react-router-dom";
+import getmascotas from "../../Actions/getmascotas";
+import Card from "../Card/Card";
+import NavBar from "../NavBar/NavBar";
+import stl from './HomeGatos.module.css';
 
-export default function HomeGatos () {
+
+
+const HomeGatos = () => {
+
+    const dispatch = useDispatch();
+    const allCats = useSelector((state)=> state.animales);
+
+    useEffect(()=>{
+        dispatch(getmascotas())
+    }, [dispatch])
 
     return (
 
-        <div>Listado de gatos</div>
+        <div>
+            <NavBar/>
+        <div>Listado de Gatos ♥</div>
+        <div className={stl.listadoCards}>
+
+            {allCats?.map(g =>{
+                return(
+                    <Link to ={'detailCat'}>
+                        <Card
+                        id = {g.id}
+                        especie = {g.especie}
+                        nombre = {g.nombre}
+                        localidad = {g.localidad}
+                        imagen = {g.imagen}
+                        />
+                    </Link>
+                )
+            })}
+
+        </div>
+
+        </div>
     )
 }
+
+export default HomeGatos;
