9eec61b28cd591fbd956c5889f5481ac9cfd783d
agosfrixione <agosfrixione@gmail.com>
2022-11-24 15:56:04 -0300

    agrego la action get_users al index de actions

--- front/src/Actions/index.js
+++ front/src/Actions/index.js
@@ -1,4 +1,5 @@
 export const GET_MASCOTAS = "GET_MASCOTAS";
+export const GET_USERS = "GET_USERS";
 export const CREATE_USER = "CREATE_USER";
 export const CREATE_ANIMAL = "CREATE_ANIMAL";
 export const GET_ANIMAL_BY_ID = "GET_ANIMAL_BY_ID";
