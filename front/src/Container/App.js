132a5503bef9eb9e900a9e6084a9775aef07039d
NachoAbait <102827148+NachoAbait@users.noreply.github.com>
2022-11-25 20:06:12 -0300

    Agrego componente SignIn y agrego su ruta en app.

--- front/src/Container/App.js
+++ front/src/Container/App.js
@@ -6,6 +6,7 @@ import HomePerros from "../Componentes/AdoptarMascota/HomePerros";
 import HomeGatos from "../Componentes/AdoptarMascota/HomeGatos";
 import InfoProceso from "../Componentes/DonarMascota/InfoProceso";
 import FormRegistro from "../Componentes/FormRegistro/FormRegistro";
+import FormSignIn from "../Componentes/FormSignIn/FormSingIn";
 import DetalleAdopcion from "../Componentes/AdoptarMascota/DetalleAdopcion";
 import Confirmacion from "../Componentes/FormRegistro/Confirmacion";
 import TePuedeInteresar from "../Componentes/TePuedeInteresar/TePuedeInteresar";
@@ -28,13 +29,21 @@ function App() {
           <Route exact path="/adoptdog" element={<HomePerros />} />
           <Route exact path="/givepet" element={<InfoProceso />} />
           <Route exact path="/signup" element={<FormRegistro />} />
-          <Route exact path="/signin" element={<FormRegistro />} />
+          <Route exact path="/signin" element={<FormSignIn />} />
           <Route exact path="/contacto" element={<DetalleAdopcion />} />
           <Route exact path="/confirmation" element={<Confirmacion />} />
           <Route exact path="/detailDog" element={<DetallePerro />} />
           <Route exact path="/detailCat" element={<DetalleGato />} />
-          <Route exact path="/tepuedeinteresar" element={<TePuedeInteresar />} />
-          <Route exact path="/reportarmaltrato" element={<ReportarMaltrato />} />
+          <Route
+            exact
+            path="/tepuedeinteresar"
+            element={<TePuedeInteresar />}
+          />
+          <Route
+            exact
+            path="/reportarmaltrato"
+            element={<ReportarMaltrato />}
+          />
           <Route exact path="/directorio" element={<Directorio />} />
           <Route exact path="/donation" element={<Donar />} />
           <Route exact path="/reportarmascota" element={<ReportarMascota />} />
