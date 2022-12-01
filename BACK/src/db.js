28a5d105fbffa11b591d72f140b691326a7fdf9c
agosfrixione <agosfrixione@gmail.com>
2022-12-01 00:39:23 -0300

    deploy db completo

--- BACK/src/db.js
+++ BACK/src/db.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 const mongoose = require ('mongoose');
-const {PASSWORD} = process.env;
+const {PASSWORD, DB_DEPLOY} = process.env;
 
-const uri = `mongodb+srv://noesneda:${PASSWORD}@proyecto.hbyeplt.mongodb.net/?retryWrites=true&w=majority`
+// const uri = `mongodb+srv://noesneda:${PASSWORD}@proyecto.hbyeplt.mongodb.net/?retryWrites=true&w=majority`
 
-mongoose.connect(uri, {
+mongoose.connect(DB_DEPLOY, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }, console.log('DB is conected'))
